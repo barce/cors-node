0d824a7923b7bb786493bdae64d2bc664706857a
Barce <barce@radiumone.com>
2012-06-11 18:57:15 -0700

    scripts for setting up a simple use case

---
+++ my_api.js
@@ -0,0 +1,41 @@
+// var agent = require('webkit-devtools-agent');
+
+var http = require('http'),
+    formidable = require('formidable'),
+    express = require('express'),
+    server;
+
+
+var app = module.exports = express.createServer();
+app.use(express.bodyParser());
+
+http.globalAgent.maxSockets = 10;
+
+app.listen(3003);
+
+app.get('/', function(req, res) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(
+              "<!DOCTYPE html>\n" +
+              '<html lang="en" id="node-api">' +
+              '<head>' +
+              '<meta charset="utf-8">' +
+              '<title>api</title>' +
+              '</head>' +
+              '<body>api</body></html>'
+    );
+});
+
+app.post('/post', function(req, res) {
+
+  console.log('got post');
+  console.log(req.body);
+  res.send(req.body.stuff, 200);
+  res.end();
+  return;
+
+
+});
+
+console.log("Express server listening on port %d in %s mode with a pid of %d", app.address().port, app.settings.env, process.pid);
+
---
+++ my_proxy.js
@@ -0,0 +1,61 @@
+// var agent = require('webkit-devtools-agent');
+
+var http = require('http'),
+    express = require('express'),
+    server;
+
+
+var app = module.exports = express.createServer();
+app.use(express.bodyParser());
+
+http.globalAgent.maxSockets = 10;
+
+app.listen(8080);
+
+
+app.get('/', function(req, res) {
+    res.writeHead(200, {'content-type': 'text/html'});
+    var mem = process.memoryUsage();
+    res.end(
+      'proxy'
+    );
+});
+
+app.post('/post', function(req, response) {
+
+  var options = {
+    host: 'l33tcave.com',
+    port: 3003,
+    path: req.path,
+    headers: req.headers,
+    method: req.method,
+    url: req.url,
+  };
+  var data = '';
+  var api_req = http.request(options, function(res) {
+    console.log("6 Got response again: " + res.statusCode);
+    res.on('error', function(e) {
+      console.log('api failure');
+      airbrake.notify(e);
+      response.writeHead(res.statusCode, res.headers);
+      response.end(res.body);
+
+    });
+
+    res.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    res.on('end', function() {
+      response.writeHead(res.statusCode, res.headers);
+      response.end(data);
+    });
+
+  });
+  api_req.write(qs.stringify(req.body));
+  api_req.end();
+
+
+});
+console.log("Express server listening on port %d in %s mode with a pid of %d", app.address().port, app.settings.env, process.pid);
+
